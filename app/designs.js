fc9f34af8d67c9b49dfa08ab0fbd6ff58744c0bf
Jordan Stubblefield <jstubblefield7939@gmail.com>
2018-01-22 14:34:10 +0000

    Revert multi-line jQuery callbacks to separate functions

--- app/designs.js
+++ app/designs.js
@@ -10,19 +10,22 @@ $('#sizePicker').submit(function(e) {
 });
 
 let color = $('#colorPicker').val() || '#000';
-$('#colorPicker').change(e => {
-  color = e.target.value;
-  if (erasing) toggleErase();
-});
+$('#colorPicker').change(e => changeColor(e.target.value));
 
 let erasing = false;
 
-$('#pixel_canvas').on('mousedown', 'td', e => {
-  $(e.target).css('background-color', erasing ? "" : color);
-  $('td').mouseover(e => $(e.target).css('background-color', erasing ? "" : color)); // this enables drag-to-brush while the user is clicking down
-});
-
+$('#pixel_canvas').on('mousedown', 'td', e => paintCanvas(e.target));
 $('#pixel_canvas').on('mouseup', 'td', e => $('td').off('mouseover'));// this cancels the brush when the user lets up on the mouse click
+
+function paintCanvas(pixel) {
+  $(pixel).css('background-color', erasing ? "" : color);
+  $('td').mouseover(e => $(e.target).css('background-color', erasing ? "" : color)); // this enables drag-to-brush while the user is clicking down
+}
+
+function changeColor(color) {
+  color = color;
+  if (erasing) toggleErase();
+}
 
 function toggleErase() {
   erasing = !erasing;
