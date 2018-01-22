2399b5d7c2366d6d3dbabce8ad426a42eef81781
Jordan Stubblefield <jstubblefield7939@gmail.com>
2018-01-22 14:46:20 +0000

    Create makeGrid function

--- app/designs.js
+++ app/designs.js
@@ -1,25 +1,26 @@
-$('#sizePicker').submit(function(e) {
-  e.preventDefault();
-
-  let rowCount = $('#input_height').val();
-  let colCount = $('#input_width').val();
-  let row = '<tr>' + '<td>'.repeat(colCount);
-  let grid = row.repeat(rowCount);
-
-  $('#pixel_canvas').html(grid);
-});
+$('#sizePicker').submit(makeGrid(e));
 
 let color = $('#colorPicker').val() || '#000';
-$('#colorPicker').change(() = e => changeColor(e.target.value));
+$('#colorPicker').change(changeColor(e.target.value));
 
 let erasing = false;
 
-$('#pixel_canvas').on('mousedown', 'td',(() =  e => paintCanvas(e.target));
-$('#pixel_canvas').on('mouseup', 'td', (() = e => $('td').off('mouseover'));// this cancels the brush when the user lets up on the mouse click
+$('#pixel_canvas').on('mousedown', 'td', paintCanvas(e.target));
+$('#pixel_canvas').on('mouseup', 'td', e => $('td').off('mouseover'));// this cancels the brush when the user lets up on the mouse click
+
+function makeGrid(e) {
+  e.preventDefault();
+  let rowCount = $('#input_height').val();
+  let colCount = $('#input_width').val();
+  let row = '<tr>' + '<td>'.repeat(colCount);
+  let grid = row.repeat(rowCount);
+  
+  $('#pixel_canvas').html(grid);
+}
 
 function paintCanvas(pixel) {
   $(pixel).css('background-color', erasing ? "" : color);
-  $('td').mouseover((() = e => $(e.target).css('background-color', erasing ? "" : color)); // this enables drag-to-brush while the user is clicking down
+  $('td').mouseover(e => $(e.target).css('background-color', erasing ? "" : color)); // this enables drag-to-brush while the user is clicking down
 }
 
 function changeColor(color) {
