5a06b1cc06380bf99ce6ab23011a4434399ba573
Jordan Stubblefield <jstubblefield7939@gmail.com>
2018-01-23 22:39:52 -0500

    Put js in an IIFE for anonymity and private variables

--- app/designs.js
+++ app/designs.js
@@ -1,36 +1,39 @@
-$('#sizePicker').submit(function(e) {
-  e.preventDefault();
+(function() {
+  $('#sizePicker').submit(function(e) { e.preventDefault(); makeGrid(); });
 
-  let rowCount = $('#input_height').val();
-  let colCount = $('#input_width').val();
-  let row = '<tr>' + '<td>'.repeat(colCount);
-  let grid = row.repeat(rowCount);
+  let color = $('#colorPicker').val() || '#000';
+  let erasing = false;
 
-  $('#pixel_canvas').html(grid);
-});
+  function makeGrid() {
+    let rowCount = $('#input_height').val();
+    let colCount = $('#input_width').val();
+    let row = '<tr>' + '<td>'.repeat(colCount);
+    let grid = row.repeat(rowCount);
 
-let color = $('#colorPicker').val() || '#000';
-$('#colorPicker').change(e => {
-  color = e.target.value;
-  if (erasing) toggleErase();
-});
+    $('#pixel_canvas').html(grid);
+  }
 
-let erasing = false;
+  $('#colorPicker').change(e => {
+    color = e.target.value;
+    if (erasing) toggleErase();
+  });
 
-$('#pixel_canvas').on('mousedown', 'td', e => {
-  $(e.target).css('background-color', erasing ? "" : color);
-  $('td').mouseover(e => $(e.target).css('background-color', erasing ? "" : color)); // this enables drag-to-brush while the user is clicking down
-});
 
-$(body).on('mouseup', 'td', e => $('td').off('mouseover'));// this cancels the brush when the user lets up on the mouse click
+  $('#pixel_canvas').on('mousedown', 'td', e => {
+    $(e.target).css('background-color', erasing ? "" : color);
+    $('td').mouseover(e => $(e.target).css('background-color', erasing ? "" : color)); // this enables drag-to-brush while the user is clicking down
+  });
 
-function toggleErase() {
-  erasing = !erasing;
-  $('#eraser').toggleClass('btn-secondary');
-  $('#eraser').toggleClass('btn-warning');
-}
+  $('#pixel_canvas').on('mouseup', 'td', e => $('td').off('mouseover'));// this cancels the brush when the user lets up on the mouse click
 
-function clearGrid() {
-  $('td').css('background-color', "");
-  if (erasing) toggleErase();
-}
+  function toggleErase() {
+    erasing = !erasing;
+    $('#eraser').toggleClass('btn-secondary');
+    $('#eraser').toggleClass('btn-warning');
+  }
+
+  function clearGrid() {
+    $('td').css('background-color', "");
+    if (erasing) toggleErase();
+  }
+})();
