ca39384faaad3714977103d35aea1ba2ba419b8c
Jordan Stubblefield <jstubblefield7939@gmail.com>
2018-01-23 22:33:46 -0500

    Revert to before functions; something broke

--- app/designs.js
+++ app/designs.js
@@ -1,38 +1,28 @@
-let color = $('#colorPicker').val() || '#000';
-let erasing = false;
-
-$('#sizePicker').submit(function(e) { makeGrid(e) });
-$('#colorPicker').change(function(e) {changeColor(e.target.value) });
-$('#pixel_canvas').on('mousedown', 'td', function(e) {paintCanvas(e.target) });
-$('body').on('mouseup', 'td', stopPainting());// this cancels the brush when the user lets up on the mouse click
-
-function makeGrid(e) {
+$('#sizePicker').submit(function(e) {
   e.preventDefault();
-  let rowCount = $('#input_height').val();
+
+  let rowCount = $('#input_height').val();
   let colCount = $('#input_width').val();
   let row = '<tr>' + '<td>'.repeat(colCount);
   let grid = row.repeat(rowCount);
-  
-  $('#pixel_canvas').html(grid);
-}
 
-function paintCanvas(pixel) {
-  colorPixel(pixel)
-  $('td').mouseover(colorPixel(e.target)); // this enables drag-to-brush while the user is clicking down
-}
+  $('#pixel_canvas').html(grid);
+});
 
-function colorPixel(pixel) {
-  $(pixel).css('background-color', erasing ? "" : color);
-}
-
-function stopPainting() {
-  $('td').off('mouseover');
-}
-
-function changeColor(color) {
-  color = color;
+let color = $('#colorPicker').val() || '#000';
+$('#colorPicker').change(e => {
+  color = e.target.value;
   if (erasing) toggleErase();
-}
+});
+
+let erasing = false;
+
+$('#pixel_canvas').on('mousedown', 'td', e => {
+  $(e.target).css('background-color', erasing ? "" : color);
+  $('td').mouseover(e => $(e.target).css('background-color', erasing ? "" : color)); // this enables drag-to-brush while the user is clicking down
+});
+
+$(body).on('mouseup', 'td', e => $('td').off('mouseover'));// this cancels the brush when the user lets up on the mouse click
 
 function toggleErase() {
   erasing = !erasing;
